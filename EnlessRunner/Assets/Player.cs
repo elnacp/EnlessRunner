713ebc976ddd5d4537b69d92a5221775f269e93d
elnacabot <ls30917@salleurl.edu>
2018-10-16 02:02:05 +0200

    First Video
    From the tutorial of Blackthorprod

---
+++ EnlessRunner/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Vector2 targetPosition;
+    public float Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+   	
+	void Update () {
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed);
+    
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight) { 
+            targetPosition = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            
+        }else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight) { 
+            targetPosition = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            
+        }
+
+    
+	}
+}
