aae4c502820d1290f727aa9b771d56024507c270
elnacabot <ls30917@salleurl.edu>
2018-10-16 15:46:20 +0200

    Finish Second Video
    NEW
    
    Object Pattern
    Spawner

---
+++ EnlessRunner/Assets/Obstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public int damage = 1;
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("TOCO");
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Player>().health -= damage;
+            Destroy(gameObject);  //elimina el obstacle
+        }
+
+    }
+
+
+}
--- EnlessRunner/Assets/Player.cs
+++ EnlessRunner/Assets/Player.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
-    private Vector2 targetPosition;
+    private Vector3 targetPosition;
     public float Yincrement;
     public float speed;
     public float maxHeight;
     public float minHeight;
-   	
+
+
+    public int health = 3;
+
 	void Update () {
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed);
+        if( health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     
         if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight) { 
-            targetPosition = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            targetPosition = new Vector3(transform.position.x, transform.position.y + Yincrement);
             
         }else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight) { 
-            targetPosition = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            targetPosition = new Vector3(transform.position.x, transform.position.y - Yincrement);
             
         }
 
